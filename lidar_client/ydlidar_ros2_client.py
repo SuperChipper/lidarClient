b232976e7b462df890a03e221b28f0fda397dbe8
Chip <1625703948@qq.com>
2024-06-05 23:38:43 +0800

    data_test

--- lidar_client/ydlidar_ros2_client.py
+++ lidar_client/ydlidar_ros2_client.py
@@ -2,9 +2,9 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from math import pi
-
+#import numpy as np
 def RAD2DEG(x):
-    return x * 180.0 / pi
+    return int(x * 180.0 / pi)
 
 class YDLidarROS2DriverClient(Node):
 
@@ -19,12 +19,12 @@ class YDLidarROS2DriverClient(Node):
 
     def scan_cb(self, scan):
         count = int(scan.scan_time / scan.time_increment)
-        self.get_logger().info(f'I heard a laser scan {scan.header.frame_id}[{count}]:')
-        self.get_logger().info(f'angle_range : [{RAD2DEG(scan.angle_min)}, {RAD2DEG(scan.angle_max)}]')
+        #self.get_logger().info(f'I heard a laser scan {scan.header.frame_id}[{count}]:')
+        #self.get_logger().info(f'angle_range : [{RAD2DEG(scan.angle_min)}, {RAD2DEG(scan.angle_max)}]')
 
-        for i in range(count):
-            degree = RAD2DEG(scan.angle_min + scan.angle_increment * i)
-            self.get_logger().info(f'angle-distance : [{degree}, {scan.ranges[i]}]')
+        
+        print('\r'+str(RAD2DEG(scan.angle_min + scan.angle_increment * 180))+','+str(scan.ranges[180])+','+str(len(scan.ranges))+','+str(scan.angle_increment),end='')
+            #self.get_logger().info(f'angle-distance : [{degree}, {scan.ranges[i]}]')
 
 def main(args=None):
     rclpy.init(args=args)
