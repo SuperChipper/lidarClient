52de95502acbc5793eca1db5aff27dda76c35ae6
Chip <1625703948@qq.com>
2024-06-12 20:26:38 +0800

    readme

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ This package contains a few strategies to create service clients.
 The `client` recipe shows how to request data from a service with a blocking call.
 The `client_async` recipe shows how to request data from a service with a non blocking call. The user has to check if a response has been received in the main loop
 The `client_async_member_function` recipe is analog to `client_async` but sends the request inside a MinimalClient class
+### 雷达中心指向-180,逆时针为正，共计404个点360度
+```bash
+source ./install/setup.bash
+ros2 run lidar_client client
+```
--- lidar_client/ydlidar_ros2_client.py
+++ lidar_client/ydlidar_ros2_client.py
@@ -1,8 +1,14 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Path
 from math import pi
-#import numpy as np
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
+from tf_transformations import euler_from_quaternion
+import numpy as np
+
+real_pose_2d=[0.,0.,0.]
+
 def RAD2DEG(x):
     return int(x * 180.0 / pi)
 
@@ -22,9 +28,28 @@ class YDLidarROS2DriverClient(Node):
         #self.get_logger().info(f'I heard a laser scan {scan.header.frame_id}[{count}]:')
         #self.get_logger().info(f'angle_range : [{RAD2DEG(scan.angle_min)}, {RAD2DEG(scan.angle_max)}]')
 
+        pick_num=int(scan.angle_min+len(scan.ranges)*real_pose_2d/360)%len(scan.ranges)
         
-        print('\r'+str(RAD2DEG(scan.angle_min + scan.angle_increment * 180))+','+str(scan.ranges[180])+','+str(len(scan.ranges))+','+str(scan.angle_increment),end='')
-            #self.get_logger().info(f'angle-distance : [{degree}, {scan.ranges[i]}]')
+        if pick_num<4 or pick_num>len(scan.ranges)-4:
+            window=np.array(scan.ranges[pick_num-2:]+scan.ranges[0:(pick_num+2)%len(scan.ranges)])
+        else:
+            window=np.array(scan.ranges[pick_num-2:pick_num+2])
+        print('\r'+str(RAD2DEG(pick_num))+','+str(scan.ranges[pick_num])+','+str(len(scan.ranges))+','+str(np.mean(window)),end='')
+
+class Subscriber_RealPath(Node):
+    def __init__(self):
+        super().__init__('Subscriber_RealPath')
+        self.subscription = self.create_subscription(Path, 'r1_py_run_pkg/real', self.listener_callback, 10, callback_group=ReentrantCallbackGroup())
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        global real_pose_2d
+        real_pose_2d[0]=float(msg.poses[-1].pose.position.x)
+        real_pose_2d[1]=float(msg.poses[-1].pose.position.y)
+        q = msg.poses[-1].pose.orientation
+        # _, _, real_location_list[2] = tf2_geometry_msgs.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])
+        (roll, pitch, yaw_sub) = euler_from_quaternion([q.x, q.y, q.z, q.w])
+        real_pose_2d[2] = int(np.rad2deg(yaw_sub))            #self.get_logger().info(f'angle-distance : [{degree}, {scan.ranges[i]}]')
 
 def main(args=None):
     rclpy.init(args=args)
